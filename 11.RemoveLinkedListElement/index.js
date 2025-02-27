4379e6cc1c6e6ea1ea1ddaa1dd86bd47f18cb5d0
Kuba <jakub.jaszcz538@gmail.com>
2025-02-27 21:09:35 +0100

    Removing elements from linked list finished

---
+++ 11.RemoveLinkedListElement/index.js
@@ -0,0 +1,24 @@
+var ListNode = /** @class */ (function () {
+    function ListNode(val, next) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+    return ListNode;
+}());
+function removeElements(head, val) {
+    if (!head) {
+        return head;
+    }
+    var curr = new ListNode(-1); //Creating a helper node -1 (an element which is befor list)
+    curr.next = head;
+    head = curr; // head is now pointer to curr
+    while (curr.next !== null) {
+        if (curr.next.val === val) { //if next value is === value 
+            curr.next = curr.next.next; // we skip this element by setting pointer to the next ListNode
+        }
+        else {
+            curr = curr.next; //if the value is not found we go to the next element
+        }
+    }
+    return head.next; //We have to return head.next becasue current element returns -1 and then rest of the list
+}
